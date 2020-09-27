ebffef14a9e069248f89013141177e945795a5fe
gppandey123 <gppandey9919@gmail.com>
2020-09-27 18:20:39 +0530

    Updates

---
+++ precache-manifest.34d95eec2c6633a3383cd68ed8cdfa59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b5dfc530d28fe94f88896b946e4bba",
+    "url": "/chess-board-1/index.html"
+  },
+  {
+    "revision": "9e268590afc91dd428a9",
+    "url": "/chess-board-1/static/css/main.82c4c878.chunk.css"
+  },
+  {
+    "revision": "caefc5c13ca488cb425a",
+    "url": "/chess-board-1/static/js/2.f2809e41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chess-board-1/static/js/2.f2809e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e268590afc91dd428a9",
+    "url": "/chess-board-1/static/js/main.26e5d584.chunk.js"
+  },
+  {
+    "revision": "c5fa6327c88ec15ee569",
+    "url": "/chess-board-1/static/js/runtime-main.3fa8775a.js"
+  }
+]);
